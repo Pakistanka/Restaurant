4c73623c9acf91f91e1f575c2ec1f28ead9dd614
Anna <polozova.anastasi@gmail.com>
2018-05-16 10:58:19 +0300

    fixed a mistake

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,6 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-//    return `http://localhost:${port}/data/restaurants.json`;
     return `http://localhost/sw/data/restaurants.json`;
   }
 
